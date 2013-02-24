b5aa20dbb237d645b13dffaa23cc8f768cc774fb
Sebastian Otto <=sotto@wemakeweb.de>
2013-02-24 02:16:19 +0100

    create db views foreach model attribute
    Signed-off-by: Sebastian Otto <=sotto@wemakeweb.de>

--- yolo/src/startup.js
+++ yolo/src/startup.js
@@ -1,5 +1,4 @@
 var fs = require('fs'),
-	_ = require('underscore'),
 	formatName = function(str){
 		return str.charAt(0).toUpperCase() + str.slice(1).replace('.js', '');
 	};
@@ -22,15 +21,92 @@ module.exports = {
 		}
 	},
 
-	//TODO: loadModels and loadControllers are the same we should merge this maybe?
 	loadModels : function(){
 		var path = APP + 'models/',
 			models = fs.readdirSync(path),
 			l = {};
 
 		models.forEach(function(model){
-			var name = formatName(model);
-			l[name] = require(path + model); 
+			var name = formatName(model),
+				Model = require(path + model),
+				model_instance = new Model(),
+				model_proto = Model.prototype,
+				views = {};
+
+			/* 
+				Each model have to inherit from the Yolo.Model
+			*/
+			if( !(model_instance instanceof Yolo.Model)){
+				return Yolo.logger.warn("Model '" + name + "' expected to be instance of Yolo.Model");
+			} 
+
+			if( !'model_name' in model_instance ){
+				return Yolo.logger.warn("Model '" + name + "' must provied property 'model_name'");
+			}
+
+			views = {
+				findById : {
+					map : "function(doc){ if(doc.type === '"+model_proto.model_name+"'){ emit(doc._id, doc);}}"
+				}
+			};
+
+			//generate views foreach attribute
+			_.each(model_instance.defaults, function(v, attribute){
+				views["findBy" + formatName(attribute) ] = {
+					map : 'function(doc){ if(doc.type === "'+model_proto.model_name+'"){ emit(doc.'+attribute+', doc);}}'
+				};
+			});
+
+			//collect view methods
+			views = _.extend({}, model_instance.views, views);
+
+			//save view methods to db
+			Yolo.db.save('_design/' + model_proto.model_name, views);
+
+			//replace the methods with functions for calling the db
+			_.each(views, function(methods, viewName){
+				//assign each view as static method to the model
+				Model[viewName] = function(options, cb, ctx){
+					//if theres no callback we do nothing
+					if (!cb || !cb.call){
+						return;
+					}
+
+					if(_.isString(options)){
+						options = {key: options};
+					}
+
+					//call the view
+					Yolo.db.view(model_proto.model_name + '/' + viewName, options || {}, function(err, result){
+						var res = [];
+						
+						if(err){
+							//handle error
+							//TODO what should we do with database errors?
+							return;
+						}
+							
+						//we loop over each item in db result and create a class instance with the result values
+						for(var i = 0, len = result.length, item = result[i], Model; i < len; i++, item = result[i]){
+
+							//lockup the model							
+							if( (Model = Yolo.models[formatName(item.value.type)]) ){
+								//type is only for db storing and referncing back to the model
+								delete item.value.type;
+								//push the created model to the result
+								res.push( new Model(item.value) );
+							}
+						}
+
+						cb.call(ctx || this, res);
+					});
+				};
+			});
+
+			//delete the the views object
+			delete Model.prototype.views;
+
+			l[name] = Model;
 		});
 
 		return l;
