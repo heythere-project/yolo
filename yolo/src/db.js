713577f17272a7295a712084b720fd0352719516
Sebastian Otto <=sotto@wemakeweb.de>
2013-02-24 02:17:32 +0100

    better error handling
    Signed-off-by: Sebastian Otto <=sotto@wemakeweb.de>

--- yolo/src/db.js
+++ yolo/src/db.js
@@ -1,9 +1,23 @@
 var cradle = require('cradle');
 
 var connection = new(cradle.Connection)('localhost', 5984, {
-  cache: false,
-  raw: false
-});
+  		cache: false,
+  		raw: false
+	}),
+	db = connection.database(Yolo.config.database.name);
 
-module.exports = connection.database(Yolo.config.database.name);
+	db.exists(function(err, exists){
+		if(err && err.code === 'ECONNREFUSED'){
+			Yolo.logger.error("Couldnt connect to Database");
+			Yolo.logger.log("Stopping Yolo…");
+			process.exit(1);
+		}
+
+		if(!exists){
+			Yolo.logger.warn("Database '" + Yolo.config.database.name + "' was created");
+			db.create();
+		} 
+	})
+
+module.exports = db;
 
