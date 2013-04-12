df9d2a1297aeac9e8dfa412e1a17208a482daf49
Sebastian Otto <sotto@wemakeweb.de>
2013-04-12 13:55:52 +0200

    fixes #4, fixes #6

---
+++ src/error.js
@@ -0,0 +1,36 @@
+module.exports = {
+	400 : {
+		code : 400,
+		message : "Bad Request"
+	},
+
+	401 : {
+		code : 401,
+		message : "Unauthorized"
+	},
+
+	403 : {
+		code : 403,
+		message : "Forbidden"
+	},
+
+	404 : {
+		code : 404,
+		message : "Not Found"
+	},
+
+	405 : {
+		code : 405,
+		message : "Method Not Allowed"
+	},
+
+	409 : {
+		code : 409,
+		message : "Conflict"
+	},
+
+	500 : {
+		code : 500,
+		message : "Internal Server Error"
+	}
+};
